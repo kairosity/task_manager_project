81b267bf61ca98aeb5397e1cc9f2752c9f2e720c
kairosity <karinafinegan@gmail.com>
2021-01-11 20:22:25 +0000

    add task form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -14,3 +14,15 @@ $(document).ready(function(){
 $(document).ready(function(){
     $('.tooltipped').tooltip();
   });
+
+$(document).ready(function(){
+    $('.datepicker').datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
+  });
+          
